dd9575224cb898fca2013ce89cf09a757e84ca8e
alireza <alireza.abdolalipoor@gmail.com>
2020-01-02 19:51:03 +0330

    refactor app build.gradle

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,16 @@
+object Dependencies {
+    val appComapat = "androidx.appcompat:appcompat:1.0.2"
+    val core = "androidx.core:core-ktx:1.0.2"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
+
+object Versions {
+    val kotlin = "1.3.50"
+}
+
+object TestDependencies {
+    val junit = "junit:junit:4.12"
+    val junitExt = "androidx.test.ext:junit:1.1.0"
+    val espresso = "androidx.test.espresso:espresso-core:3.1.1"
+}
